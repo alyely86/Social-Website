0da6478dda146e4ae070cca44aea35324f1c6dda
aly <alyasyly868@gmail.com>
2025-01-09 11:27:47 -0800

    Implement user authentication with login and logout functionality, add dashboard view, and enhance base template with user-specific navigation.

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import *
 
 urlpatterns = [
-	path('login/',user_login,name='login'),
+    # previous login url
+	# path('login/',user_login,name='login'),
+
+    #login / logout urls
+    path('login/',auth_views.LoginView.as_view(),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+
+    path('', dashboard, name='dashboard'),
 ]
