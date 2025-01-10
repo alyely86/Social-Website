83aef79bd0a661d2ce3209b6622679f6f15e83d5
aly <alyasyly868@gmail.com>
2025-01-10 04:56:05 -0800

    Add password reset and change functionality with corresponding templates

--- account/urls.py
+++ account/urls.py
@@ -9,6 +9,19 @@ urlpatterns = [
     #login / logout urls
     path('login/',auth_views.LoginView.as_view(),name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+    #change password urls
+    path('password-change/',auth_views.PasswordChangeView.as_view(),name='password_change'),
+    path('password-change/done/',auth_views.PasswordChangeDoneView.as_view(),name='password_change_done'),
+
+    #reset password urls
+    path('password-reset/',
+        auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete/',
+        auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 
     path('', dashboard, name='dashboard'),
+
 ]
