3318f2a8c4647bcbc5b97c548c7b14d2d60258ea
aly <alyasyly868@gmail.com>
2025-01-11 20:47:54 -0800

    Add success and error messages for profile updates in the edit view

--- account/views.py
+++ account/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate,login
 from .forms import LoginForm,UserRegistrationForm,\
     UserEditForm,ProfileEditForm
 from .models import Profile
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -67,7 +68,8 @@ def register(request):
 def edit(request):
     if request.method == 'POST':
         user_form = UserEditForm(
-            instance=request.user,data=request.POST
+            instance=request.user,
+            data=request.POST
         )
         profile_form = ProfileEditForm(
             instance=request.user.profile,
@@ -77,6 +79,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated '\
+                'successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(
