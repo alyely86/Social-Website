9b05829051ba52f30a602845cc0cd5c77d0e953c
aly <alyasyly868@gmail.com>
2025-02-02 12:21:00 -0800

    Add user list and detail views with templates for user profiles

--- account/urls.py
+++ account/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
     path('',include('django.contrib.auth.urls')),
     path('', dashboard, name='dashboard'),
     path('register/',register,name='register'),
-    path('edit/',edit,name='edit')
+    path('edit/',edit,name='edit'),
+    path('users/', user_list, name='user_list'),
+    path('users/<username>/', user_detail, name='user_detail'),
 
 ]
--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
 from .forms import LoginForm,UserRegistrationForm,\
@@ -93,3 +94,22 @@ def edit(request):
             {'user_form': user_form,
             'profile_form': profile_form}
         )
+
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(request,
+        'account/user/list.html',
+        {'section': 'people',
+        'users': users})
+
+@login_required
+def user_detail(request,username):
+    user = get_object_or_404(User,
+        username=username,is_active = True                     
+        )
+    return render(request,
+        'account/user/detail.html',
+        {'section': 'people',
+        'user': user})
