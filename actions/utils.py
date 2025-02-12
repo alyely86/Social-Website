65b212b2c971bb5d90377c81825d0e632a3fb971
aly <alyasyly868@gmail.com>
2025-02-12 15:52:24 -0800

    Add action deduplication to prevent duplicate entries within one minute

--- actions/utils.py
+++ actions/utils.py
@@ -1,6 +1,28 @@
 from django.contrib.contenttypes.models import ContentType
 from .models import Actions
+import datetime
+from django.utils import timezone
 
 def create_action(user,verb,target=None):
+    # check for any similar action made in the last minute
+    now = timezone.now()
+    last_minute = now - datetime.timedelta(seconds=60)
+    similar_actions = Actions.objects.filter(user_id = user.id,
+        verb = verb,created__gta = last_minute
+    )
+    
+    if target:
+        target_ct = ContentType.objects.get_for_model(target)
+        similar_actions = similar_actions.filter(
+            target_ct=target_ct,
+            target_id=target.id)
+    if not similar_actions:
+
+        #no existing acions found
+        action = Actions(user= user,verb=verb,target =target)
+        action.save()
+        return True
+    return False
+
     action = Actions(user=user, verb=verb, target=target)
     action.save()
