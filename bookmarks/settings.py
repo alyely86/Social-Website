95905ba000a5e2a80caa3cde1ab8d4eb681d7c2a
aly <alyasyly868@gmail.com>
2025-02-14 15:25:45 -0800

    Add Django Debug Toolbar to settings and URLs for improved debugging

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -44,9 +44,11 @@ INSTALLED_APPS = [
     'django_extensions',
     'easy_thumbnails',
     'actions.apps.ActionsConfig',
+    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -164,3 +166,7 @@ if DEBUG:
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user':lambda u:reverse_lazy('user_detail',args =[u.username])
 }
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
